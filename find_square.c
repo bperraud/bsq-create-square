fbdb3d1fb233a0dd4c78cffcedeb7d1ffd36e4c7
bperraud <74benji@live.fr>
2021-11-29 16:50:16 +0100

    commit du covid

--- find_square.c
+++ find_square.c
@@ -1,19 +1,68 @@
+#include <stdbool.h>
 #include "header.h"
-
-
-
 
 
 t_point* find_biggest_square(t_map map)
 {
-
-
+    t_point point;
 
 
 }
 
 
+/*
+ * return 1 if there are "lines" empty lines from start point
+ */
+int is_empty_lines(t_map map, t_point start_point, int lines)
+{
+    int i;
 
+    i = 0;
+    while (i < lines)
+    {
+        if (!is_empty_line(map, start_point, lines))
+            return (0);
+        else
+            i++;
+    }
+    return (1);
+}
+
+/*
+ * return 1 if the line of start_point is empty for size char
+ */
+int is_empty_line(t_map map, t_point point, int nb_symbol)
+{
+    int i;
+
+    i = 0;
+    while (i < nb_symbol)
+    {
+
+    }
+
+}
+
+
+/*
+ * try to create a square of size 1 at start_point
+ */
+int try_nsquare(t_map map, t_point start_point, int n)
+{
+    int i;
+
+    i = 0;
+    while( i < n)
+    {
+        if (is_empty_lines(map, start_point, n))
+
+        i++;
+    }
+}
+
+/*
+ * get point with coordinate (x, y)
+ */
 t_point get_point(t_map map, int x, int y)
 {
     int i;
@@ -25,6 +74,14 @@ t_point get_point(t_map map, int x, int y)
             return map.points[i];
     }
     return map.points[0];
+}
+
+/*
+ * get index in tab of points of map of the point in parameter
+ */
+int get_point_index(t_map map, t_point to_find_point)
+{
+    return -1;
 }
 
 
--- header.h
+++ header.h
@@ -10,6 +10,15 @@ typedef struct s_point
     int symbol; //0 vide 1 plein 2 obstacle
 }   t_point;
 
+
+typedef struct s_square
+{
+    t_point start_point;
+    t_point end_point;
+    int size;
+} t_square;
+
+
 typedef struct s_map
 {
     t_point *points;
@@ -17,7 +26,13 @@ typedef struct s_map
     int lenght;
     int nb_points;
     char symbols[3];   // 3 indices
+    t_square best_square;
 } t_map;
 
+
+
+//functions
+int is_empty_line(t_map map, t_point point, int lines);
+int is_empty_lines(t_map map, t_point start_point, int lines);
 
 #endif
